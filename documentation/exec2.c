a16de7235b9db61d786a83707b108c3ae3d6505d
Igor Ruiz <ruizigor08@gmail.com>
2026-05-15 22:08:42 -0300

    adicionando documentação exercício 2

---
+++ documentation/exec2.c
@@ -0,0 +1,20 @@
+/*
+Escreva a documentação correta da função abaixo.
+*/
+
+/*
+A função abaixo recebe um vetor v[1...n], o valor da quantidade de elementos
+n > 0 do vetor e um valor x ∈  v[1...n] e devolve a soma J > 0 de 1 em 1
+até que seja j < n e v[j] ≠ x
+*/
+int onde (int x, int v[], int n){
+
+	int j = 0;
+
+	while (j < n && v[j] != x){
+
+		j += 1;
+	}
+
+	return j;
+}
